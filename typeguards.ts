4b7885c772c95ee21521384b5ddd5f20c6c8161e
Mel <rodriguezml291@gmail.com>
2019-08-29 23:37:00 -0600

    type guards

---
+++ typeguards.ts
@@ -0,0 +1,28 @@
+interface Bird{
+    fly();
+    layEggs();
+}
+
+interface Fish{
+    swim();
+    layEggs();
+}
+
+function getAnimal():Fish|Bird{
+    var a:Fish;
+    return a;
+}
+
+function isFish(pet:Fish|Bird):pet is Fish{
+    return (<Fish>pet).swim != undefined;
+}
+
+let pet = getAnimal();
+
+if(isFish(pet)){
+    (<Fish>pet).swim();
+    console.log('gulp');
+}else{
+    console.log('believe i can fly');
+    pet.fly();
+}
