ec6f84e116ae87b928eb87e4a7ca93d254217427
Mel <rodriguezml291@gmail.com>
2019-08-29 23:31:44 -0600

    type alias

---
+++ typealias.js
@@ -0,0 +1,14 @@
+function getName(n) {
+    if (typeof n === "string") {
+        return n;
+    }
+    else {
+        return n();
+    }
+}
+var FooIdBrand;
+(function (FooIdBrand) {
+})(FooIdBrand || (FooIdBrand = {}));
+var BarIdBrand;
+(function (BarIdBrand) {
+})(BarIdBrand || (BarIdBrand = {}));
