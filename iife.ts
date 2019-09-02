e7dbb55de5c573af7b75f83438858d5299308407
Mel <rodriguezml291@gmail.com>
2019-09-01 22:05:29 -0600

    iife

---
+++ iife.ts
@@ -0,0 +1,29 @@
+// Clase padre
+class Point {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    }
+    add(point: Point) {
+    return new Point(this.x + point.x, this.y + point.y);
+    }
+}
+
+    // clase hija
+    class Point3D extends Point {
+    z: number;
+    constructor(x: number, y: number, z: number) {
+    super(x, y);
+    this.z = z;
+    }
+    // función add
+    add(point: Point3D) {
+        // llamada a la función del padre
+        var point2D = super.add(point);
+        // devolvemos resultado
+        return new Point3D(point2D.x, point2D.y, this.z + point.z);
+    }
+    }
+    
