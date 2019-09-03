310b9166cdf022b50658e9cf4703da9aaa98eb1a
Mel <rodriguezml291@gmail.com>
2019-09-02 21:19:10 -0600

    ecmascript 6

---
+++ importado.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var SECRETO = "Me gustan los robots";
+var opciones = {
+    color: 'rojo',
+    imagen: false
+};
+function suma(a, b) {
+    return a + b;
+}
+exports["default"] = {
+    SECRETO: SECRETO,
+    opciones: opciones,
+    suma: suma
+};
---
+++ importado.ts
@@ -0,0 +1,13 @@
+const SECRETO = "Me gustan los robots";
+var opciones = {
+    color: 'rojo',
+    imagen: false,
+};
+function suma(a, b) {
+    return a + b;
+}
+export default {
+    SECRETO,
+    opciones,
+    suma,
+};
